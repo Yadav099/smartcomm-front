8b5dcfff8cdf92705d4d1a8d2a42b1d6390ba5e2
Madan R <madan.r@hashedin.com>
2020-03-05 19:45:40 +0530

    worked on signup page and app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Signup } from './Container/Signup';
+
 
 function App() {
   return (
     <div className="App">
+      <Signup/>
     </div>
   );
 }
--- src/Container/Signup/index.tsx
+++ src/Container/Signup/index.tsx
@@ -1,71 +1,118 @@
 import React from  'react'
-import { Button } from '../../Component/Button';
-import { FileInput } from '../../Component/FileInput';
-import { TextBox } from '../../Component/TextBox';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import axios from 'axios'
 
-//interface to store the data inserted on the signup page
-interface Registration{
-    companyName :string
-    companyMail :string
-    employeeName :string
-    employeeMail: string
-    employeeID: string
-    configFile: File
-}
 
-//function to store the register
-export const Signup: React.FC = () => {
-    const[companyName,updateCompanyName]=React.useState("");
+
+export const Signup = () => {
+
+  const[companyName,updateCompanyName]=React.useState("");
     const[employeeName,updateEmployeeName]=React.useState("");
     const[companyMail,updateCompanyEmail]=React.useState("");
     const[employeeMail,updateEmployeeEmail]=React.useState("");
     const[employeeID,updateEmployeeID]= React.useState("")
-    const[configFile,updateConfigFile]=React.useState();
-    
-    const AddUser = () => {
-        const entry: Registration = {companyName,companyMail,employeeName,employeeMail,employeeID,configFile};
-        console.log(entry);
-    };
+  
 
-    const onUpdateCompanyName = (name:string) =>{
-        updateCompanyName(name);
-    }
-    const onUpdateCompanyMail = (mailID:string) =>{
-        updateCompanyEmail(mailID);
-    }
-    const onUpdateEmployeeName = (name:string) =>{
-        updateEmployeeName(name);
-    }
-    const onUpdateEmployeeMail = (mail:string) =>{
-        updateEmployeeEmail(mail);
-    }
-    const onUpdateEmployeeID = (ID:string) =>{
-        updateEmployeeID(ID);
-    }
-    const onUpdateConfigFile = (e: React.ChangeEvent<HTMLInputElement>) =>{
-        updateConfigFile(e.target.files);
-    }
-    return(
-        <div>
-            <h2>SignUp</h2>
-            <div>
-                <section className="companyData">
-                    <h3>COMPANY</h3>
-                    <TextBox placeHolder="Name" updater={onUpdateCompanyName} />
-                    <TextBox placeHolder="Email" updater={onUpdateCompanyMail}/>
-                    <FileInput uplodeFile={onUpdateConfigFile} />
-                </section>
-                <section className="EmployeeData">
-                    <h3>EMPLOYEE</h3>
-                    <TextBox placeHolder="Name" updater={onUpdateEmployeeName}/>
-                    <TextBox placeHolder="Email" updater={onUpdateEmployeeMail}/>
-                    <TextBox placeHolder="ID.No." updater={onUpdateEmployeeID}/>
-                </section>
-            </div>
-            <Button onClick={AddUser}>
-                Add Entry
-            </Button>
-        </div>
-    );
+  const useStyles = makeStyles(theme => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: 350,
+      },
+    },
+  }));
+  const classes = useStyles()
+
+  const handleClick= () =>{
+    console.log("post")
+    axios.post('http://tradenapp-env.us-east-1.elasticbeanstalk.com/api/v1/api-token-auth/', {
+            companyname:companyName,
+            companyemail:companyMail,
+            employeename:employeeName,
+            employeeid:employeeID,
+            employeemail:employeeMail
+
+          })
+          .then(function (response) {
+            console.log(response.status);
+            if(response.status==200)
+            {
+              alert("success")      
+            }
+            console.log(response.data)
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+          
+        console.log("Login Button Pressed")
+  }
+    
+return(
+    <Container>
+  <Row>
+  <Navbar className="justify-content-center" id="color-nav" fixed="top" style={{height:"60px",fontSize:"30px" }}>Smart Comm</Navbar>
+  </Row>
+  <br/>
+  <br/>
+  <Container className="main-signup">
+  <Container className="main-signup">
+    <br/>
+    <br/>
+  <br/>
+  <br/>
+  <br/>
+    <Row>
+    <h3 style={{height:"20px" }}>Company</h3>
+    </Row>
+    <br/>
+   <Row>
+  <form className={classes.root} noValidate autoComplete="off">
+  <TextField id="outlined-basic" label="Company Name"  variant="outlined" value={companyName} onChange={(e) => updateCompanyName(e.target.value)}/>
+  <br />
+  <TextField id="outlined-basic" label="Company Email" variant="outlined" value={companyMail} onChange={(e) => updateCompanyEmail(e.target.value)}/>
+</form>
+</Row> 
+  
+  </Container>
+  <br/>
+  <br/>
+  <Container>
+    <br/>
+    <Row>
+    <h3 style={{height:"20px" }}>Employee</h3>
+    </Row>
+    <br/>
+   <Row>
+  <form className={classes.root} noValidate autoComplete="off">
+  <TextField id="outlined-basic" label="Employee Name"  variant="outlined" value={employeeName} onChange={(e) => updateEmployeeName(e.target.value)}/>
+  <br />
+  <TextField id="outlined-basic" label="Employee ID" variant="outlined" value={employeeID} onChange={(e) => updateEmployeeID(e.target.value)}/>
+  <br />
+  <TextField id="outlined-basic" label="Employee Email" variant="outlined" value={employeeMail} onChange={(e) => updateEmployeeEmail(e.target.value)}/>
+</form>
+</Row> 
+  
+  </Container>
+  <br/>
+
+  <Row>
+    <Button onClick={handleClick} style={{width:"10em" ,backgroundColor:"#45398B",color:"white"}}>Sign Up</Button>
+  </Row>
+  </Container>
+  
+</Container>
+)
 
 }
+
+
+
+
