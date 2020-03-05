9656533e82304786d8bdd707189c4a9b07d6c0af
Yadav Padiyar M <yadav.padiyar@hashedin.com>
2020-03-05 15:21:34 +0530

    Updated component and container directory

--- src/Component/Button/index.tsx
+++ src/Component/Button/index.tsx
@@ -1,16 +1,15 @@
-import * as React from 'react';
-import { ButtonHTMLAttributes } from 'react';
-
+import * as React from "react";
+import { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    customClassName?: string;
+  customClassName?: string;
 }
 
 export const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
-    const { color, customClassName, ...btnProps } = props;
-    return (
-        <button className="btn btn-secondary" {...btnProps} >
-            {props.children}
-        </button>
-    );
+  const { color, customClassName, ...btnProps } = props;
+  return (
+    <button className="btn btn-secondary" {...btnProps}>
+      {props.children}
+    </button>
+  );
 };
