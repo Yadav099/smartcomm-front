edfde5fc9b2a49c54e637cc9699325be24bf7c34
Madan R <madan.r@hashedin.com>
2020-03-12 16:25:14 +0530

    added following components

---
+++ src/Component/AddCustomer/index.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Row from "react-bootstrap/Row";
+import {
+  Paper,
+  withStyles,
+  Grid,
+  TextField,
+  Button,
+  FormControlLabel,
+  Checkbox,
+  Input
+} from "@material-ui/core";
+
+import { Typography, makeStyles, Theme, createStyles } from "@material-ui/core";
+import { Container, Col } from "react-bootstrap";
+interface data {
+
+  name: String;
+  age: string;
+  sex: String;
+  email: String;
+  phonenumber: string;
+  interest: String;
+}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    textField: {
+      width: "300px"
+    },
+    space: {
+      margin: theme.spacing(1)
+    },
+    FieldName: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "flex-end"
+    },
+    displayBox: {
+      width: "400px",
+      height: "300px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }
+  })
+);
+export const AddCustomer = () => {
+  const Fileds = ["Name", "Age", "sex", "email", "Phone Number", "interest"];
+  const classes = useStyles();
+  const [name, setName] = React.useState("");
+  const [age, setAge] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phonenumber, setPhoneNumber] = React.useState("");
+  const [sex, setSex] = React.useState("");
+  const [interest, setInterest] = React.useState("");
+  var jsondata :data[]=[] ;
+ 
+
+  const handleDataChange = (data: string, e: any) => {
+    if (data === "Name") {
+      setName(e.target.value);
+      console.log(name);
+    }
+    if (data === "Age") {
+      setAge(e.target.value);
+      console.log(age);
+    }
+    if (data === "sex") {
+      setSex(e.target.value);
+      console.log(sex);
+    }
+    if (data === "email") {
+      setEmail(e.target.value);
+      console.log(email);
+    }
+    if (data === "Phone Number") {
+      setPhoneNumber(e.target.value);
+      console.log(phonenumber);
+    }
+    if (data === "interest") {
+      setInterest(e.target.value);
+      console.log(interest);
+    }
+  };
+  return (
+    <Container>
+      <Row>
+        <form noValidate autoComplete="off">
+          {Fileds.map((text, index) => (
+            <Row className="justify-content-md-center">
+              <Col className={classes.FieldName}>
+                <Typography>{text}:</Typography>
+              </Col>
+              <Col className={classes.textField}>
+                <TextField
+                  label={text}
+                  onChange={e => {
+                    handleDataChange(text, e);
+                  }}
+                />
+              </Col>
+            </Row>
+          ))}
+        </form>
+      </Row>
+      <br />
+      <br />
+      <Row className="justify-content-md-center">
+      <Button onClick={()=> { 
+          var temp=name;
+          
+          
+          
+          
+          jsondata.push({name:temp,age:age,sex:sex,email:email,phonenumber:phonenumber,interest:interest});console.log(jsondata)}} >submit</Button>
+      </Row>
+      <br />
+      <br />
+      {/* <Row className="justify-content-md-center">
+        <Paper elevation={3} className={classes.displayBox}>
+          {
+              jsondata.map((element:data) => {return(<>
+              <p>{element.age}</p> 
+             </>
+               ) })
+          }
+        </Paper>
+      </Row> */}
+
+      <br />
+      <br />
+      <Row>
+        <Input name="upload-csv-file" type="file"  />
+      </Row>
+    </Container>
+  );
+};
--- src/Container/Home/index.tsx
+++ src/Container/Home/index.tsx
@@ -1,60 +1,169 @@
-import React from "react";
-import "./main.scss";
-import SideNavBar from "../../Component/SideNavBar";
-import AppBar from "@material-ui/core/AppBar";
 
-import { Typography, makeStyles, Theme, createStyles } from "@material-ui/core";
-import { Container } from "react-bootstrap";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import EmailEditor from "../../Component/EmailEditor";
+import { CustomerPage } from "../../Component/CustomerPage"
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    HomeWrapper: {
-      display: "flex",
-      flexDirection: "column"
+const drawerWidth = 260;
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
     },
-    AppBar: {
-      backgroundColor: "#45398B",
-      padding: "1em"
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: "#45398B"
+  },
+  title: {
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
     },
-    "@media(width:762px) ": {
-      AppBar: {
-        width: "100rem"
-      }
-    }
-  })
-);
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  closeMenuButton: {
+    marginRight: 'auto',
+    marginLeft: 0,
+  },
+}));
+function Home() {
 
-const Home = () => {
-  const [body, setBody] = React.useState(0);
+  const [value, setValue] = React.useState(1);
+  // const dummyCategories = ['Send', 'Customer', 'Logout']
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen)
+  }
 
-  const homeStyle = useStyles();
+  const handleClick1 = () => {
+    setValue(1)
+    console.log(value)
+  }
+  const handleClick2 = () => {
+    setValue(2)
+    console.log(value)
+  }
+  const handleClick3 = () => {
+    setValue(3)
+    console.log(value)
+  }
+
   const send = (filter: number, content: string) => {
-    alert(filter + content);
-  };
+         alert(filter + content);
+  }
+const drawer = (
+    <div>
+      <List>
+        {/* {dummyCategories.map((text, index) => ( */}
+          <ListItem button key='Send' onClick={handleClick1} >
+            <ListItemText primary='Send' />
+          </ListItem>
 
-  return (
-    <Container className={homeStyle.HomeWrapper}>
-      <AppBar className={homeStyle.AppBar} position="static">
-        <Typography variant="h6" className={"title"}>
-          Smart Comm
-        </Typography>
-      </AppBar>
-      <body className="homePageWrapper">
-        <SideNavBar setBody={setBody} />
+          <ListItem button key='Customer' onClick={handleClick2} >
+            <ListItemText primary='Customer' />
+          </ListItem>
 
-        {body === 0 ? (
-          <>
-            <EmailEditor send={send} />
-          </>
-        ) : body === 1 ? (
-          <b>New users</b>
-        ) : (
-          <b>Customer</b>
-        )}
-      </body>
-    </Container>
+          <ListItem button key='Logout' onClick={handleClick3} >
+            <ListItemText primary='Logout' />
+          </ListItem>
+        {/* ))} */}
+      </List>
+    </div>
   );
-};
+return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap className={classes.title}>
+            Smart Comm
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <nav className={classes.drawer}>
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            <IconButton onClick={handleDrawerToggle} className={classes.closeMenuButton}>
+              <CloseIcon/>
+            </IconButton>
+            {drawer}
+          </Drawer>
+        </Hidden>
+<Hidden xsDown implementation="css">
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.toolbar} />
+            {drawer}
+          </Drawer>  
+        </Hidden>
+      </nav>
+      <div className={classes.content}>
+        <div className={classes.toolbar} /> 
+        {value === 1 && <EmailEditor send={send} /> }
+        {value === 2 && <CustomerPage/> }
+        {value === 3 && <p> page 3</p> }
+
+      </div>
+    </div>
+  );
+}
 
 export default Home;
